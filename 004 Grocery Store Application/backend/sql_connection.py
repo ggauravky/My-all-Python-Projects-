e755fa201d0de36550c3201150f7c2047cf1a7c7
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-10-05 20:02:26 +0530

    created backendlogic for product add , delete and show product details

---
+++ 004 Grocery Store Application/backend/sql_connection.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+__cnx=None
+
+def get_sql_connection():
+    global __cnx
+    if __cnx is None:
+        __cnx=mysql.connector.connect(user='root', password='gaurav@sql',
+                                host='127.0.0.1',
+                                database='grocery store application')
+        
+    return __cnx
+        
